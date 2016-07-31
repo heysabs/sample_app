afd9886b0c1d10e10c101de00a34c561a6082c8b
Sabrina Ooi <heysabs@gmail.com>
2016-07-31 22:35:23 +0800

    add user following and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,14 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
   # Follows a user.
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
